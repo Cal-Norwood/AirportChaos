e1bd83ede055f98f030a8e343bfb1c0279ae5013
Cal-Norwood <146857953+Cal-Norwood@users.noreply.github.com>
2024-04-16 12:43:43 +0100

    e

---
+++ My project/Assets/Scripts/FPSMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    [SerializeField]private Rigidbody rb;
+    private float x;
+    private float y;
+    public float speed;
+    public bool isGrounded = false;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(isGrounded == true)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce((x * transform.right + y * transform.forward) * speed, ForceMode.VelocityChange);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
